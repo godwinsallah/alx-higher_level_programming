e172a18a09a2a35ebd864718723f5552dc31b457
godwinsallah <godwinsallah16@gmail.com>
2023-08-13 20:36:15 +0000

    2

---
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+#2-replace_in_list.py
+
+def replace_in_list(my_list, idx, element):
+    """Replace a specified element in the list"""
+    if idx < 0 or idx >= len(my_list):
+        pass
+    else:
+        my_list[idx] = element
+    return my_list
