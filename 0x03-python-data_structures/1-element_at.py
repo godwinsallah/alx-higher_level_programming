a268323b1b814546ddf18f0d6c6c794a36712ad4
godwinsallah <godwinsallah16@gmail.com>
2023-08-13 20:08:30 +0000

    1

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -4,9 +4,7 @@ def element_at(my_list, idx):
     """Prints an element of
     a list based on the index
     """
-    for i in range(len(my_list)):
-        if i >= 0 and i < len(my_list):
-            if i == idx:
-                print("{}".format(my_list[i]))
+        if idx < 0 or idx >= len(my_list):
+            return (None)
         else:
-            print(None)
+            return (my_list[idx])
