e35346cf9aad97c71d2c9ddebccb4d216717e8c2
godwinsallah <godwinsallah16@gmail.com>
2023-08-16 14:27:11 +0000

    0

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -2,9 +2,9 @@
 #0-square_matrix_simple.py
 
 def square_matrix_simple(matrix=[]):
+    """squares all element of the matrix"""
     new_matrix = matrix.copy()
 
     for i in range(len(matrix)):
         new_matrix[i] = list(map(lambda x: x**2, matrix[i]))
-
     return (new_matrix)
